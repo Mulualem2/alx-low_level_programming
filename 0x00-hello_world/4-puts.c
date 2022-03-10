8cfed0865596c5b3a0b5daca1463b0536c27e200
Mulualem2 <mulualemasfaw2321@gmail.com>
2022-03-10 06:36:41 -0800

    using puts() function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 #include <string.h>
+/**
+  *main -entry point
+  *
+  *return: always 0
+  */
 int main(void)
 {
 	char str[55];
-	strcpy(str, "\"Programming is like building a multilingual puzzle");
+
+	strcpy(str,"\"Programming is like building a multilingual puzzle");
         puts(str);
-	return(0);
+	return (0);
 }
