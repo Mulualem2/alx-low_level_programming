1243400168c33dd8559626af092ef377c3bacd8e
Mulualem2 <mulualemasfaw2321@gmail.com>
2022-03-11 07:29:12 -0800

    combinations

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,12 +10,15 @@ int main(void)
 {
 	int c;
 
-	for (c = '0'; c <= '8'; ++c)
+	for (c = '0'; c <= '9'; ++c)
 	{
 		putchar(c);
-		putchar(',');
-		putchar(' ');
+		if (c < '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
-	putchar('9');
+	putchar('\n');
 	return (0);
 }
