4207ce71424e7ff1bdd63e8b2e55eebfa894bac3
Mulualem2 <mulualemasfaw2321@gmail.com>
2022-03-11 02:55:02 -0800

    print numbers using putchar()

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+  *main - entry point
+  *
+  *Return: Always 0 (success)
+  */
+int main(void)
+{
+	int c;
+
+	for (c = '0'; c <= '9'; ++c)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
