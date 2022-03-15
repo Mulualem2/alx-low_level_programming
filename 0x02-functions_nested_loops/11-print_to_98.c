f47902b74a0ac09822b2bcd2b782a5b07313539c
Mulualem2 <mulualemasfaw2321@gmail.com>
2022-03-15 06:20:22 -0700

    print up to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,8 +1,8 @@
 #include "main.h"
 #include <stdio.h>
 /**
-  *main - entry point
-  *
+  *print_to_98 - print up to 98
+  *@n: int type number
   *Description: prints n to 98
   */
 void print_to_98(int n)
