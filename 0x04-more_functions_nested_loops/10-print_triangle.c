4c1c480da00f99150a7553a0eff727123aaf90b3
Mulualem Mekonnen <88831286+Mulualem2@users.noreply.github.com>
2022-03-17 16:18:32 +0300

    Update 10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -11,25 +11,15 @@ void print_triangle(int size)
 	int i, j;
 
 	if (size > 0)
-	{
 		for (i = size; i > 0; i--)
 		{
 			for (j = 1; j <= size; j++)
-			{
-				if (j >= 1)
-				{
+				if (j >= i)
 					_putchar('#');
-				}
 				else
-				{
 					_putchar(' ');
-				}
-				_putchar('\n');
-			}
+			_putchar('\n');
 		}
-	}
 	else
-	{
-	_putchar('\n');
-	}
+		_putchar('\n');
 }
