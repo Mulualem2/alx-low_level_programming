3fe00b1484d8a84cda2d54ebf039e40c91830a11
Mulualem Mekonnen <88831286+Mulualem2@users.noreply.github.com>
2022-03-17 16:44:54 +0300

    Update 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,29 +11,23 @@ int main(void)
 {
 	int i;
 
-	for (i = 1; i <= 100; i++)
+	i = 1;
+	while (i <= 100)
 	{
-		if ( i % 3 == 0 && i % 5  == 0)
-		{
+		if (i % 3 == 0 && i % 5 == 0)
 			printf("FizzBuzz");
-		}
 		else if (i % 3 == 0)
-		{
 			printf("Fizz");
-		}
 		else if (i % 5 == 0)
-		{
 			printf("Buzz");
-		}
 		else
-		{
 			printf("%d", i);
-		}
+
 		if (i != 100)
-		{
 			printf(" ");
-		}
+		i++;
 	}
 	printf("\n");
+
 	return (0);
 }
