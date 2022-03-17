486afc426d3a41326667cd40fd811aec0c7e6bd7
Mulualem2 <mulualemasfaw2321@gmail.com>
2022-03-17 07:16:15 -0700

    functions and nested loops

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <math.h>
+/**
+  *main - entry point
+  *
+  *Return: 0
+  */
+
+int main(void)
+{
+	int i;
+	long x = 612852475143;
+
+	for (c = (int) sqrt(x); c > 2; c++)
+	{
+		if (x % i == 0)
+		{
+			printf("%d\n", i);
+			break;
+		}
+	}
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,45 @@
+#include "main.h"
+/**
+  *print_number - print an intiger
+  *@n: integer number
+  *Return: void
+  */
+
+void print_number(int n)
+{
+	long x;
+	long y;
+	int z;
+
+	x = n;
+
+	if (x < 0)
+	{
+		x *= -1;
+		_putchar('-');
+	}
+
+	y = 1;
+	z = 1;
+	while (z)
+	{
+		if (x / (y * 10) > 0)
+			y *= 10;
+		else
+			z = 0;
+	}
+
+	while (x >= 0)
+	{
+		if (y == 1)
+		{
+			_putchar(x % 10 + '0');
+			x = -1;
+		}
+		else
+		{
+			_putchar((x / y % 10) + 0);
+			y /= 10;
+		}
+	}
+}
