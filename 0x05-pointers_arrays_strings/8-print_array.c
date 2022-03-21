29ce37b7dcbb4601c9c18d94d7912220fdfe7add
Mulualem2 <mulualemasfaw2321@gmail.com>
2022-03-21 04:54:48 -0700

    pointers

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+  *print_array - prints array elements separated by comma
+  *@a: int type pointer
+  *@n: int type number
+  *Return: void
+  */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n - 1; i++)
+	{
+		printf("%d, ", a[i]);
+
+			if (i == (n -1))
+				printf("%d", a[n - 1]);
+	}
+	printf("\n");
+}
