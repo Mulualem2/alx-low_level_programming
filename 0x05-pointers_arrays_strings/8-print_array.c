5f5d83e8f4b100c2393ca7e8636d65ae0cb39c4c
Mulualem2 <mulualemasfaw2321@gmail.com>
2022-03-21 04:59:21 -0700

    pointers

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -16,8 +16,8 @@ void print_array(int *a, int n)
 	{
 		printf("%d, ", a[i]);
 
-			if (i == (n -1))
-				printf("%d", a[n - 1]);
+			if (i == (n - 1))
+				printf("%d", a[i]);
 	}
 	printf("\n");
 }
