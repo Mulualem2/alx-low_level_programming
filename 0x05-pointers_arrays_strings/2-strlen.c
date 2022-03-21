916a758566c70f29e79d42b607fb81bc4b90282d
Mulualem2 <mulualemasfaw2321@gmail.com>
2022-03-21 03:01:55 -0700

    pointers

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+  *_strlen - returns length of the string passed
+  *@s: char type pointer
+  *Return: string length
+  */
+
+int _strlen(char *s)
+{
+	int x;
+
+	for (x =0; s[x] != '\0'; x++)
+		;
+
+	return (x);
+}
