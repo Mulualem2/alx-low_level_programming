45185f79968b132cf96a1a966a811f41cd4e6afc
Mulualem2 <mulualemasfaw2321@gmail.com>
2022-03-21 03:30:46 -0700

    pointers

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+  *print_rev - reverse a string
+  *@s: char typr pointer
+  *Return: void
+  */
+
+void print_rev(char *s)
+{
+	int x, y;
+	int len = 0;
+
+	for (x =0; s[x] ! '\0'; x++)
+	{
+		len++;
+	}
+
+	for (y = (len - 1); y >= 0; y--)
+	{
+		_putchar(s[y]);
+	}
+	_putchar('\n');
+}
