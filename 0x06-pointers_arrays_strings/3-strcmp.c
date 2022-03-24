10843db4569c8aa9fe2bf66897dbbaa07e41330e
Mulualem Mekonnen <88831286+Mulualem2@users.noreply.github.com>
2022-03-25 00:23:23 +0300

    pointers

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,1 +1,20 @@
+#include "main.h"
 
+/**
+ * _strcmp - Compare strings
+ * @s1: char type pointyer
+ * @s2: char type pointer
+ * Return: neg if s1 < s2, 0 if equal, and pos int if s1 > s2
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+	}
+	return (0);
+}
