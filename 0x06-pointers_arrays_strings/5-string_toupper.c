01c9842949002a0aa10099d4086edf1b3ba1d610
Mulualem Mekonnen <88831286+Mulualem2@users.noreply.github.com>
2022-03-25 00:27:28 +0300

    pointers

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,1 +1,18 @@
-i
+#include "main.h"
+
+/**
+ * string_toupper - lower to upper
+ * @n: char type pointer
+ * Return: string
+ */
+char *string_toupper(char *n)
+{
+	int i;
+
+	for (i = 0; n[i] != '\0'; i++)
+	{
+		if (n[i] >= 'a' && n[i] <= 'z')
+			n[i] = n[i] - 32;
+	}
+	return (n);
+}
