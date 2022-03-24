4631c3468a2cc6dca14fee7f0c111961ae0c7956
Mulualem Mekonnen <88831286+Mulualem2@users.noreply.github.com>
2022-03-25 00:30:39 +0300

    pointers

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,1 +1,34 @@
-i
+#include "holberton.h"
+
+/**
+ * cap_string - capitalizes all words of a string
+ * @n: char type pointer
+ * Return: caps on first letter of a separator
+ */
+char *cap_string(char *n)
+{
+	int i, x;
+	int c = 32;
+	int sep[] = {',', ';', '.', '?', '"',
+			    '(', ')', '{', '}', ' ', '\n', '\t'};
+
+	for (i = 0; n[i] != '\0'; i++)
+	{
+		if (n[i] >= 'a' && n[i] <= 'z')
+		{
+			n[i] = n[i] - c;
+		}
+
+		c = 0;
+
+		for (x = 0; x <= 12; x++)
+		{
+			if (n[i] == sep[x])
+			{
+				x = 12;
+				c = 32;
+			}
+		}
+	}
+	return (n);
+}
