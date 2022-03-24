552f37172f551efc22c8b45164aa3039c8d5c6f4
Mulualem Mekonnen <88831286+Mulualem2@users.noreply.github.com>
2022-03-25 01:07:48 +0300

    pointers

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,1 +1,24 @@
-i
+#include "main.h"
+
+/**
+ * rot13 - rotate characters 13 places in the alphabet
+ * @s: string pointer
+ * Return: string s
+ */
+
+char *rot13(char *s)
+{
+	int i;
+	char st1[] = "NOPQRSTUVWXYZABCDEFGHIJKLM";
+	char st2[] = "nopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if ((s[i] > 64 && s[i] < 91) || (s[i] > 96 && s[i] < 123))
+		{
+			s[i] = (s[i] - 65 > 25) ?
+				st2[s[i] - 97] : st1[s[i] - 65];
+		}
+	}
+	return (s);
+}
