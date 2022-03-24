ceafb33f715ed9c707262a380bbe5fec568b5522
Mulualem Mekonnen <88831286+Mulualem2@users.noreply.github.com>
2022-03-25 01:36:14 +0300

    pointers

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -5,25 +5,15 @@
  *
  * Return: string after conversion of 'leet'
  */
-char *leet(char *n)
+char *leet(char *s)
 {
-	int i, x;
-	int find[] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
-	int replacer[] = {'4', '3', '0', '7', '1'};
+	int i, j;
+	char letters[] = "aAeEoOtTlL";
+	char leet_it[] = "4433007711";
 
-	for (i = 0; n[i] != '\0'; i++)
-	{
-		for (x = 0; x <= 9; x++)
-		{
-			if (n[i] == find[x])
-			{
-				n[i] = replacer[x / 2];
-				x = 9;
-			}
-		}
-	}
-
-	return (n);
-}
+	for (i = 0; s[i] != '\0'; i++)
+		for (j = 0; j < 10; j++)
+			if (s[i] == letters[j])
+				s[i] = leet_it[j];
 	return (s);
 }
