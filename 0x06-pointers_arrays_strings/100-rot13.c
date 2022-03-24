2c37793f6811cd2e3a3f52a9bb47de84be1c6f6a
Mulualem Mekonnen <88831286+Mulualem2@users.noreply.github.com>
2022-03-25 01:37:52 +0300

    pointers

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -8,17 +8,13 @@
 
 char *rot13(char *s)
 {
-	int i;
-	char st1[] = "NOPQRSTUVWXYZABCDEFGHIJKLM";
-	char st2[] = "nopqrstuvwxyzabcdefghijklm";
+	int i, j;
+	char alphabet[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char rot_it[] =   "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
 
 	for (i = 0; s[i] != '\0'; i++)
-	{
-		if ((s[i] > 64 && s[i] < 91) || (s[i] > 96 && s[i] < 123))
-		{
-			s[i] = (s[i] - 65 > 25) ?
-				st2[s[i] - 97] : st1[s[i] - 65];
-		}
-	}
+		for (j = 0; j < 52; j++)
+			if (s[i] == alphabet[j])
+				s[i] = rot_it[j], j = 52;
 	return (s);
 }
