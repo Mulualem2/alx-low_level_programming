20eee3418ecb7c877d0281f19ab811c6ba2cfbf0
Mulualem Mekonnen <88831286+Mulualem2@users.noreply.github.com>
2022-03-25 00:25:02 +0300

    pointers

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,1 +1,15 @@
-i
+#include "main.h"
+/**
+ * reverse_array - reverses array of integers
+ * @a: int type pointer
+ * @n: length of array
+ *
+ * Return: void
+ */
+void reverse_array(int *a, int n)
+{
+	int j, temp, i = n;
+
+	for (i--, j = 0; j < n / 2; j++, i--)
+		temp = a[j], a[j] = a[i], a[i] = temp;
+}
