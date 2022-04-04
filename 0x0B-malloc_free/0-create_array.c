aeaa4c9c285ad129c54b2a62dc2d34567027584e
Mulualem2 <mulualemasfaw2321@gmail.com>
2022-04-04 06:00:56 -0700

    malooc free

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /**
- * create_array - creates an array of chars, and initializes it with a specific char
+ * create_array - creates an array of chars
  * @size: size input
  * @c: char
  * Return: 0
