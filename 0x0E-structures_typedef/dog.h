f0be99a45118a87c4924926b8e528c9878e41ca5
Mulualem2 <mulualemasfaw2321@gmail.com>
2022-04-11 10:58:25 -0700

    commited

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,26 +2,15 @@
 #define DOG_H
 
 /**
- * struct dog - a dog
- *
- * @name: char type variable
- * @age: float type variable
- * @owner:char type variable
+ * struct dog - structure definition of a dog
+ * @name: character string
+ * @age: integer
+ * @owner: character string
  */
 typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-} dog;
-
-typedef dog dog_t;
-
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
-
-int _putchar(char c);
-
+};
 #endif
