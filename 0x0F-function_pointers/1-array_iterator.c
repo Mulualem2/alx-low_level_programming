e9a2dbec24c0c8c463458d036276bf6488c654e4
Mulualem2 <mulualemasfaw2321@gmail.com>
2022-04-13 07:52:35 -0700

    function pointers

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,6 +11,11 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
 
+	if (array == NULL)
+		return;
+	if (action == NULL)
+		return;
+
 	for (i = 0; i < size; i++)
 		action(array[i]);
 }
