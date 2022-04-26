48e2c9117723048ae6ea209304cd157c67801308
Mulualem2 <mulualemasfaw2321@gmail.com>
2022-04-26 12:41:15 -0700

    singly linked list

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,10 +1,10 @@
 #include "lists.h"
-
 /**
- * listint_len - a function that returns the number of elements in a linked listint_t list
- * @h: linked list head
- * Return: number of elements in linked list
- */
+* listint_len - a function that returns the number
+*  of elements in a linked listint_t list
+* @h: input
+* Return: 0
+*/
 size_t listint_len(const listint_t *h)
 {
 	size_t n = 0;
