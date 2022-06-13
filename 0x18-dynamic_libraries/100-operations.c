919e7fb57c2e3185cd5e53107516266d9c7fde74
Mulualem2 <mulualemasfaw2321@gmail.com>
2022-06-13 11:24:59 -0700

    dynamic libraries

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,54 @@
+/**
+  *add - add two integers
+  *@a: integer
+  *@b: integer
+  *Return: sum
+  */
+int add(int a, int b)
+{
+    return (a+b);
+}
+
+/**
+  *sub - substruct two integers
+  *@a: integer
+  *@b: integer
+  *Return: difference
+  */
+int sub(int a, int b)
+{
+    return (a-b);
+}
+
+/**
+  *mul - multiply add two integers
+  *@a: integer
+  *@b: integer
+  *Return: product
+  */
+int mul(int a, int b)
+{
+    return (a*b);
+}
+
+/**
+  *div - divide two integers
+  *@a: integer
+  *@b: integer
+  *Return: quotient
+  */
+int div(int a, int b)
+{
+    return (a/b);
+}
+
+/**
+  *mod - modulo of numbers
+  *@a: integer
+  *@b: integer
+  *Return: result of the operation
+  */
+int mod(int a, int b)
+{
+    return (a%b);
+}
