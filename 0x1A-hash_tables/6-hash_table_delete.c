9e13064d627351a080ff1683b3ae4c920d81e71d
Mulualem2 <mulualemasfaw2321@gmail.com>
2022-07-01 04:27:16 -0700

    hash tabels in c

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -6,19 +6,25 @@
  */
 void hash_table_delete(hash_table_t *ht)
 {
-	hash_node_t *temporal, *next;
-	unsigned long int index;
+	hash_table_t *head = ht;
+	hash_node_t *node, *tmp;
+	unsigned long int i;
 
-	if (!ht)
-		return;
-	for (index = 0; index < ht->size; index++)
-		for (temporal = ht->array[index]; temporal; temporal = next)
+	for (i = 0; i < ht->size; i++)
+	{
+		if (ht->array[i] != NULL)
 		{
-			next = temporal->next;
-			free(temporal->key);
-			free(temporal->value);
-			free(temporal);
+			node = ht->array[i];
+			while (node != NULL)
+			{
+				tmp = node->next;
+				free(node->key);
+				free(node->value);
+				free(node);
+				node = tmp;
+			}
 		}
-	free(ht->array);
-	free(ht);
+	}
+	free(head->array);
+	free(head);
 }
