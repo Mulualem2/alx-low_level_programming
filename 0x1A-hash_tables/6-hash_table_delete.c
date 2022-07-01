3ec48986b9492817c412cbaeed20e3274f25fee2
Mulualem2 <mulualemasfaw2321@gmail.com>
2022-07-01 04:46:26 -0700

    hash tabels in c

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,19 +1,4 @@
-#include "hash_tables.h"
-
-/**
- * hash_chain_delete - delete a singly-linked list
- * @head: a pointer to the head of the singly-linked list
- */
-void hash_chain_delete(hash_node_t *head)
-{
-	if (head)
-	{
-		hash_chain_delete(head->next);
-		free(head->key);
-		free(head->value);
-		free(head);
-	}
-}
+#include "hash_tables"
 
 /**
  * hash_table_delete - delete given hash table
@@ -21,16 +6,19 @@ void hash_chain_delete(hash_node_t *head)
  */
 void hash_table_delete(hash_table_t *ht)
 {
-	hash_node_t **array = NULL;
-	unsigned long int index = 0;
-	unsigned long int size = 0;
+	hash_node_t *temporal, *next;
+	unsigned long int index;
 
-	if (ht)
-	{
-		for (array = ht->array, size = ht->size; index < size; ++index)
-			hash_chain_delete(array[index]);
-
-		free(ht->array);
-		free(ht);
-	}
+	if (!ht)
+		return;
+	for (index = 0; index < ht->size; index++)
+		for (temporal = ht->array[index]; temporal; temporal = next)
+		{
+			next = temporal->next;
+			free(temporal->key);
+			free(temporal->value);
+			free(temporal);
+		}
+	free(ht->array);
+	free(ht);
 }
