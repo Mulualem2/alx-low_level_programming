cca33c8a4f03fee748fae49db86fdc9adcd6010e
Mulualem2 <mulualemasfaw2321@gmail.com>
2022-10-04 02:15:07 -0700

    updating task 0

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -10,15 +10,12 @@
  */
 int linear_search(int *array, size_t size, int value)
 {
-	size_t i;
-
-	if (array == NULL || size == 0)
-		return (-1);
+	unsigned int i = 0;
 
 	for (i = 0; i < size; i++)
 	{
-		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
-		if (value == array[i])
+		printf("Value checked array[%i] = [%i]\n", i, array[i]);
+		if (array[i] == value)
 			return (i);
 	}
 	return (-1);
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -44,7 +44,6 @@ int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
-void print_array(int *array, size_t inc, size_t size);
 listint_t *jump_list(listint_t *list, size_t size, int value);
 skiplist_t *linear_skip(skiplist_t *list, int value);
 
